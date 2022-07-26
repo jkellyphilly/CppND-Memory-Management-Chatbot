b916f2a100a005ff9a364b158ec946791f016e6b
Joel Kelly <joel.kelly@getcruise.com>
2022-07-26 11:29:53 +0000

    Fix a segmentation fault bug

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -11,7 +11,8 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    delete _chatBot; 
+    // Remove the incorrect deletion of the chatBot here
+    // delete _chatBot; 
 
     ////
     //// EOF STUDENT CODE
